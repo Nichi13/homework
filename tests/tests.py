5f33af472f9f647162a1c51b03007b552be2c6fc
Nichi13 <47666044+Nichi13@users.noreply.github.com>
2019-04-17 23:32:48 +0300

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -25,89 +25,25 @@ class CaseTests (unittest.TestCase):
     #     remove_doc_from_shelf('10006')
     #     print(self.directories)
     #
-    # Хотел сделать через fixtures,  он даже подгружал, однако при print-e все равно полки были все на месте,
-    # как я понял он просто заново подгружал целый файл со всеми полками.
+
     def test_1(self):
-        self.assertEqual(directories['2'],['10006'])
-        print(directories)
+        old_dir = directories.copy()
         remove_doc_from_shelf('10006')
-        print(directories)
+        self.assertNotEqual(old_dir, directories)
 
     def test_2(self):
-        print(directories.keys())
+        old_dict = directories.copy()
         add_new_shelf('55555')
-        print(directories.keys())
+        self.assertNotEqual(old_dict, directories)
 
-    def test_3(self):
-        l=documents
-        print(l)
+    @mock.patch('app.input', return_value='10006')
+    def test_3(self, input):
+        old_docs = documents.copy()
         delete_doc()
         i=documents
-        print(i)
-        self.assertEqual(l,i)
-        #  мне кажется или в данном случае self.assertEqual не работает
+        self.assertNotEqual(old_docs,i)
 
-    def test_4(self):
-        print(directories['2'])
-        print(get_doc_shelf())
-        self.assertEqual('2',get_doc_shelf())
-
-#     Честно - очень сложно придумать примеры, тем более код написан так, что не подкопаться
-
-    def test_yandex(self):
-        API_KEY = 'trnsl.1.1.20190228T195305Z.0e420ab62864e7bd.999563fd55628362f83666c9741638399a300113'
-        URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-        text_str = 'привет'
-        params = {
-            'key': API_KEY,
-            'id': '0689baea.5c7838be.cf87f84b-1-0',
-            'lang': 'ru-en',
-            'reason': 'auto',
-            'srv': 'tr-text'}
-
-        i = {'text': text_str}
-        response = requests.post(URL, params=params, data=i)
-        print( response.status_code)
-        self.assertEqual(response.status_code, 200, 'Не соответствует код запроса')
-        t = response.json()
-        s =t['text'][0]
-        self.assertEqual(s, 'hi', 'Неверный перевод')
-        print(s)
-
-    def test_yandex2(self):
-        API_KEY = 'trnsl.1.1.20190228T195305Z.0e420ab62864e7bd.999563fd55628362f83666c9741638399a300113'
-        URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-        text_str = 'привет'
-        params = {
-            'key': API_KEY,
-            'id': '0689baea.5c7838be.cf87f84b-1-0',
-            'reason': 'auto',
-            'srv': 'tr-text'}
-
-        i = {'text': text_str}
-        response = requests.post(URL, params=params, data=i)
-        print('Bad Request', response.status_code)
-        self.assertEqual(response.status_code, 400)
-        t = response.json()
-
-
-    def test_yandex(self):
-        API_KEY = 'trnsl.1.1.20190228T195305Z.0e420ab62864e7bd.999563fd55628362f83666c9741638399a30011'
-        URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-        text_str = 'привет'
-        params = {
-            'key': API_KEY,
-            'id': '0689baea.5c7838be.cf87f84b-1-0',
-            'lang': 'ru-en',
-            'reason': 'auto',
-            'srv': 'tr-text'}
-
-        i = {'text': text_str}
-        response = requests.post(URL, params=params, data=i)
-        print( 'Доступ к яндекс запрещен, проверь API_KEY ',response.status_code)
-        self.assertEqual(response.status_code, 403)
-
-
-
-
-
+    @mock.patch('app.input', return_value='10006')
+    def test_4(self, input):
+        resalt = get_doc_shelf()
+        self.assertEqual('2',resalt)
